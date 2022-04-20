67302a32b767f794b8f13a146687c10b8efd4400
mayneva <maynevaanna@gmail.com>
2022-04-20 22:15:51 +0300

    add time

--- finnkinoscript.js
+++ finnkinoscript.js
@@ -12,13 +12,15 @@ function loadXMLDoc() {
 function myFunction(xml) {
   var i;
   var xmlDoc = xml.responseXML;
-  var table = "<tr><th>Theatre ID</th><th>Film name</th><th>Poster</th><th>Genres</th></tr>";
+  var table = "<tr><th>Theatre ID</th><th>Film name</th><th>Start time</th><th>Poster</th><th>Genres</th></tr>";
   var x = xmlDoc.getElementsByTagName("Show");
   for (i = 0; i < x.length; i++) {
     table += "<tr><td>" +
       x[i].getElementsByTagName("TheatreID")[0].childNodes[0].nodeValue +
       "</td><td>" +
       x[i].getElementsByTagName("Title")[0].childNodes[0].nodeValue +
+      "</td><td>" +
+      x[i].getElementsByTagName("dttmShowStart")[0].childNodes[0].nodeValue +
       "</td><td><img src='" +
       x[i].getElementsByTagName("EventSmallImagePortrait")[0].childNodes[0].nodeValue +
       "'/></td><td>" +
