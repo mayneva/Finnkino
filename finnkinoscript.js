cecae499e68d87b09e3796d425ce32d65d946661
mayneva <maynevaanna@gmail.com>
2022-04-21 22:17:16 +0300

    DDL selection

--- finnkinoscript.js
+++ finnkinoscript.js
@@ -1,61 +1,56 @@
 
-function loadXMLDoc() {
+function loadXMLDoc(ddlSelection) {
   var xmlhttp = new XMLHttpRequest();
   xmlhttp.onreadystatechange = function () {
     if (this.readyState == 4 && this.status == 200) {
-      myFunction(this);
+      myFunction(this, ddlSelection);
     }
   };
   xmlhttp.open("GET", "https://www.finnkino.fi/xml/Schedule/", true);
   xmlhttp.send();
 }
 
-function myFunction(xml) {
+function myFunction(xml, ddlSelection) {
   var i;
   var xmlDoc = xml.responseXML;
   var table = "<tr><th>Cinema/Theatre</th><th>Film name</th><th>Start time</th><th>Poster</th><th>Genres</th></tr>";
   var x = xmlDoc.getElementsByTagName("Show");
+
   for (i = 0; i < x.length; i++) {
-    table += "<tr><td>" +
-      x[i].getElementsByTagName("Theatre")[0].childNodes[0].nodeValue +
-      "</td><td>" +
-      x[i].getElementsByTagName("Title")[0].childNodes[0].nodeValue +
-      "</td><td>" +
-      x[i].getElementsByTagName("dttmShowStart")[0].childNodes[0].nodeValue +
-      "</td><td><img src='" +
-      x[i].getElementsByTagName("EventSmallImagePortrait")[0].childNodes[0].nodeValue +
-      "'/></td><td>" +
-      x[i].getElementsByTagName("Genres")[0].childNodes[0].nodeValue +
-      "</td></tr>";
+    if (x[i].getElementsByTagName("Theatre")[0].childNodes[0].nodeValue == ddlSelection) {
+
+      table += "<tr><td>" +
+        x[i].getElementsByTagName("Theatre")[0].childNodes[0].nodeValue +
+        "</td><td>" +
+        x[i].getElementsByTagName("Title")[0].childNodes[0].nodeValue +
+        "</td><td>" +
+        x[i].getElementsByTagName("dttmShowStart")[0].childNodes[0].nodeValue +
+        "</td><td><img src='" +
+        x[i].getElementsByTagName("EventSmallImagePortrait")[0].childNodes[0].nodeValue +
+        "'/></td><td>" +
+        x[i].getElementsByTagName("Genres")[0].childNodes[0].nodeValue +
+        "</td></tr > ";
+    }
   }
+
   document.getElementById("films").innerHTML = table;
 }
 
-var subjectObject = {
-  "Espoo": {},
-  "Omena, Espoo": {},
-  "Sello, Espoo": {},
-  "Helsinki": {},
-  "Itis, Helsinki": {},
-  "Kinopalatsi, Helsinki": {},
-  "Maxim, Helsinki": {},
-  "Tennispalatsi, Helsinki": {},
-  "Flamingo, Vantaa": {},
-  "Fantasia, Jyvaskyla": {},
-  "Scala, Kuopio": {},
-  "Kuvapalatsi, Lahti": {},
-  "Strand, Lappeenranta": {},
-  "Plaza, Oulu": {},
-  "Promenadi, Pori": {},
-  "Tampere": {},
-  "Cine Atlas, Tampere": {},
-  "Plevna, Tampere": {},
-  "Kinopalatsi, Turku": {},
-}
 
 window.onload = function () {
   var subjectSel = document.getElementById("subject");
   for (var x in subjectObject) {
     subjectSel.options[subjectSel.options.length] = new Option(x, x);
   }
-} 
+}
+
+
+var subjectObject = {
+  "Omena, Espoo": {},
+  "Sello, Espoo": {},
+  "Itis, Helsinki": {},
+  "Kinopalatsi, Helsinki": {},
+  "Maxim, Helsinki": {},
+  "Tennispalatsi, Helsinki": {},
+  "Flamingo, Vantaa": {},
+}
